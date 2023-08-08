526f9cfba1afe26a826ca1efa4fe902554e2083b
henry-r-wilson <henryrwilson24@gmail.com>
2023-08-08 19:50:09 +0100

    Updates from Filips

--- Code/Utilities/RVx1a_GenerateRawSelectedSeeds_Sub.py
+++ Code/Utilities/RVx1a_GenerateRawSelectedSeeds_Sub.py
@@ -134,7 +134,7 @@ for i in range(0,Steps):
   merged_data.drop(merged_data.index[merged_data['separation'] >= MaxDST], inplace = True) #Dropping the track segment combinations where the length of the gap between segments is too large
   merged_data.drop(['y','z','x','r_x','r_y','r_z','join_key','separation'],axis=1,inplace=True) #Removing the information that we don't need anymore
   if merged_data.empty==False:
-    merged_data.drop(merged_data.index[merged_data['Track_1'] == merged_data['Track_2']], inplace = True) #Removing the cases where Seed tracks are the same
+    merged_data.drop(merged_data.index[merged_data['Track_1'] == merged_data['Track_2']], inplace = True) #Removing the cases where Seed tracks are the same n^2-n
     merged_list = merged_data.values.tolist() #Convirting the result to List data type
     result_list+=merged_list #Adding the result to the list
   if len(result_list)>=2000000: #Once the list gets too big we dump the results into csv to save memory
